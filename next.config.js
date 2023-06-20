8f8c109a0f783bc6ba869758cf426a03a4976b17
fakg02 <faruk_fkg_02@outlook.de>
2023-06-20 20:49:01 +0200

    fix fs and dgram?

--- next.config.js
+++ next.config.js
@@ -1,15 +1,28 @@
 /** @type {import('next').NextConfig} */
 //const nextConfig = {};
+
+// const nextConfig = {
+//   webpack5: true,
+//   webpack: (config) => {
+//     config.resolve.fallback = {
+//       fs: false,
+//       dgram: false,
+//     };
+
+//     return config;
+//   },
+// };
+
 const nextConfig = {
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      dgram: false,
-    };
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+        dgram: "empty",
+      };
+    }
 
     return config;
   },
 };
-
 module.exports = nextConfig;
